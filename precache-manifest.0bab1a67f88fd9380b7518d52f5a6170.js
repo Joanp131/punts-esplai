193b2050883586377289d019086112fd1a8d4af0
Joanp131 <38050754+Joanp131@users.noreply.github.com>
2019-09-06 22:58:37 +0200

    Add files via upload

---
+++ precache-manifest.0bab1a67f88fd9380b7518d52f5a6170.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ea915f49ba3f93ae69850eca115c59",
+    "url": "/punts-esplai/index.html"
+  },
+  {
+    "revision": "d2f9c72b6f47cd9f0d6a",
+    "url": "/punts-esplai/static/css/main.cd81767d.chunk.css"
+  },
+  {
+    "revision": "29903c494d5c7a2d6564",
+    "url": "/punts-esplai/static/js/2.4a786854.chunk.js"
+  },
+  {
+    "revision": "d2f9c72b6f47cd9f0d6a",
+    "url": "/punts-esplai/static/js/main.a5a83761.chunk.js"
+  },
+  {
+    "revision": "97f7f42907a7d651dc60",
+    "url": "/punts-esplai/static/js/runtime~main.8fc1890b.js"
+  },
+  {
+    "revision": "e65d99a8a4988fa39c32afd542955533",
+    "url": "/punts-esplai/static/media/foulard.e65d99a8.jpg"
+  },
+  {
+    "revision": "73133f8a0be18e2d0ddc524fa77e0358",
+    "url": "/punts-esplai/static/media/repBack.73133f8a.jpg"
+  }
+]);
